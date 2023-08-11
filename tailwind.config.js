0d6e4e445562e7a61c080d4d5e385137f7aab79e
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-11 14:57:50 -0300

    add all colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 import colors from "tailwindcss/colors";
-//Arreglar para poder usar todos los colores de tailwind
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      base: "#D7D7D7",
-      primary: "#006473",
-      secondary: "#3B4F5F",
-      transparent: "transparent",
-      touch: "#3B4F5F20",
-      success: "#287300",
-      process: "#6B7300",
-      error: "#FF0000",
-      white: "#fff",
-      black: "#000",
-      gray: colors.gray,
+    extend: {
+      colors: {
+        base: "#D7D7D7",
+        primary: "#006473",
+        secondary: "#3B4F5F",
+        transparent: "transparent",
+        touch: "#3B4F5F20",
+        success: "#287300",
+        process: "#6B7300",
+        error: "#FF0000",
+        white: "#fff",
+        black: "#000",
+        gray: colors.gray,
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
