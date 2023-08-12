b72742ed719e540b46e3edc286afd44e5949da5b
Ramiro <ramifazio@gmail.com>
2023-08-12 00:32:37 -0300

    Add aurora (test)

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { router } from "./router";
 import { Provider } from "react-redux";
 import store from "./redux";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,13 +1,37 @@
+import React, { useEffect, useRef } from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
-import { Outlet } from "react-router-dom";
 
 export default function Root() {
+  const auroraRef = useRef(null);
+
+  useEffect(() => {
+    const aurora = auroraRef.current;
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const auroraSize = 250;
+      const auroraX = clientX - auroraSize / 2;
+      const auroraY = clientY - auroraSize / 2;
+
+      aurora.style.left = `${auroraX}px`;
+      aurora.style.top = `${auroraY}px`;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <>
+    <div className="relative h-full w-full">
+      <div className="aurora absolute" ref={auroraRef}></div>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
