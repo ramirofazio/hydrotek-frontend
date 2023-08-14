aeaf2801066c3d896fb5c0130f31e01b9198e58b
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-14 19:23:23 -0300

    ready eslint

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,15 +1,9 @@
 import { Pagination } from "../../components/Pagination.jsx";
 
-
 export default function Products() {
-
   return (
     <div className="container">
-      <Pagination
-        nButtons={5}
-        currentPage={2}
-        setPage={(p) => console.log(p)}
-      />
+      <Pagination nButtons={5} currentPage={2} setPage={(p) => console.log(p)} />
       <h1>Hola soy el Home!</h1>
     </div>
   );
