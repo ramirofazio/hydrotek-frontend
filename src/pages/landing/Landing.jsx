6176136efebcf1b2898b91e97cc596cac7a785df
Tomás Pérez <101677692+TomasPerez1@users.noreply.github.com>
2023-08-16 18:31:18 -0300

    Button hyd 7 (#4)
    * add reciclable Button & fix slint
    
    * add hover

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,8 +1,11 @@
+import Button from "../../components/Button";
+
 export default function Landing() {
   return (
     <div className="outletPrimaryContainer">
       <section className="flex h-screen w-full items-center justify-center">
-        <h1>Content</h1>
+        {/* <h1>Content</h1> */}
+        <Button text='comprar ahora'/>
       </section>
     </div>
   );
