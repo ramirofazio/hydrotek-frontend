a317f68696aa7a0bb884607e2948fd39c613ce8e
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-16 22:14:14 -0300

    add variant

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,15 +2,17 @@ import Button from "./Button.jsx";
 import ProductBorder from "../assets/productCardBorder.png";
 import { Link } from "react-router-dom";
 
-export default function ProducCard({ imgUrl, name, price, id }) {
+export default function ProductCard({ imgUrl, name, price, id, showBtn = false }) {
   return (
-    <div className="grid place-items-center p-3 gap-3">
+    <div className={`border-2 p-2 grid gap-2 ${showBtn && "place-items-center gap-3 p-3"}`}>
       <img src={imgUrl || ProductBorder} alt="imagen de producto" />
       <h1 className="">{name || "NOMBRE DEL PRODUCTO"}</h1>
       <h2 className="textGoldGradient mb-2">{price || "$99.99"}</h2>
-      <Link to={`/productDetail/${id}`}>
-        <Button text="COMPRAR AHORA" />
-      </Link>
+      { showBtn &&
+        <Link to={`/productDetail/${id}`}>
+          <Button text="COMPRAR AHORA" />
+        </Link>
+      }
     </div>
   );
 }
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,10 +1,10 @@
-import ProductCard from "../../components/ProductCard.jsx";
+
 
 export default function Landing() {
   return (
     <div className="outletPrimaryContainer">
       <section className="flex h-[400vh] w-full items-center justify-center">
-        <ProductCard />
+        <h1>CONTENT</h1>
       </section>
     </div>
   );
