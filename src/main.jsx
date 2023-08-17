5d2adcaa55e2e28b9e7ce5234d121abf45f97e5e
josesantangelo <josesantangelo93@gmail.com>
2023-08-16 21:33:59 -0300

    reacti18n setup

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,14 @@ import { Provider } from "react-redux";
 import store from "./redux";
 import "atropos/css";
 
+import './i18n';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+
       <RouterProvider router={router} />
+
     </Provider>
   </React.StrictMode>
 );
