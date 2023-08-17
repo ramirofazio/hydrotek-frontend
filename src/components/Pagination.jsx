d197bb229956fb0b95a878dc28bc441a9425e542
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-17 17:02:30 -0300

    add responsive style & fix transition

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 
 //Falta incorporar la logica del setPage, se necesita un estado (react/redux) al que cambiar
@@ -6,10 +5,10 @@ export function Pagination({ nButtons, currentPage = 0, setPage }) {
   const quantity = Array(nButtons).fill(1);
 
   return (
-    <div className="flex items-center gap-4 text-gray-700">
+    <div className="flex items-center gap-4">
       <button
         onClick={() => setPage(currentPage - 1)}
-        className="rounded p-2 hover:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-transparent"
+        className="hidden lg:inline rounded-xl p-2 text-white hover:bg-white hover:text-base disabled:text-opacity-30 disabled:hover:bg-opacity-5"
         disabled={currentPage === 0}
       >
         <ArrowLeftIcon className="h-5 w-5" />
@@ -19,10 +18,10 @@ export function Pagination({ nButtons, currentPage = 0, setPage }) {
           <button
             onClick={() => setPage(index)}
             key={index}
-            className={`grid  h-10 w-10 place-content-center rounded p-2.5 transition-all  ${
+            className={`grid  h-10 w-10 place-content-center rounded-full p-2.5 goldGradient text-white text-lg transition-all ${
               currentPage === index
-                ? "bg-primary font-medium text-white shadow shadow-primary/80 ease-in"
-                : " bg-gray-100 ease-out hover:bg-gray-300"
+                ? "font-medium  shadow shadow-primary/80 ease-in"
+                : " opacity-40 ease-out hover:opacity-60"
             }`}
           >
             <span>{index + 1}</span>
@@ -31,7 +30,7 @@ export function Pagination({ nButtons, currentPage = 0, setPage }) {
       </div>
       <button
         onClick={() => setPage(currentPage + 1)}
-        className="rounded p-2 hover:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-transparent"
+        className="hidden lg:inline rounded-xl p-2 text-white hover:bg-white hover:text-base disabled:text-opacity-30 disabled:hover:bg-opacity-5"
         disabled={currentPage === nButtons - 1}
       >
         <ArrowRightIcon className="h-5 w-5" />
@@ -39,9 +38,3 @@ export function Pagination({ nButtons, currentPage = 0, setPage }) {
     </div>
   );
 }
-
-Pagination.propTypes = {
-  nButtons: PropTypes.number.isRequired,
-  currentPage: PropTypes.number.isRequired,
-  setPage: PropTypes.func.isRequired,
-};
