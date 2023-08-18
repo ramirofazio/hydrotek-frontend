7687206b3d0c1bee125cefa210e2ca6e29e85086
Ramiro <ramifazio@gmail.com>
2023-08-18 20:32:45 -0300

    footer mobile with no  ©

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,66 +8,60 @@ import mail from "../assets/mail.png";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col border-t-8  border-gold">
-      <section className="flex h-52 justify-between px-20">
-        <section className="footerSections">
-          <>
-            <img src={textLogo} className="my-4 w-64" />
-          </>
-          <div className="flex">
-            <div className="footerNetworksIcons">
-              <img src={facebook} className="h-4" />
-            </div>
-            <div className="footerNetworksIcons">
-              <img src={instagram} className="h-4" />
-            </div>
-            <div className="footerNetworksIcons">
-              <img src={whatsapp} className="h-4" />
-            </div>
-            <div className="footerNetworksIcons">
-              <img src={mail} className="h-3" />
-            </div>
+    <footer className="h-full w-full border-t-8 border-gold">
+      <section className="grid grid-cols-1  place-items-center gap-10 p-8">
+        <img src={textLogo} className="col-span-1 w-full " />
+        <div className="flex w-full justify-around">
+          <div className="goldGradient flex h-12 w-12 items-center justify-center rounded-full">
+            <img src={facebook} className=" h-4 w-auto" />
           </div>
-        </section>
-        <section className="footerSections">
-          <h1 className="textGoldGradient">¿QUIÉNES SOMOS?</h1>
-          <p className="w-72  py-2">
+          <div className="goldGradient flex h-12 w-12 items-center justify-center rounded-full">
+            <img src={instagram} className=" h-4 w-auto" />
+          </div>
+          <div className="goldGradient flex h-12 w-12 items-center justify-center rounded-full">
+            <img src={whatsapp} className=" h-4 w-auto" />
+          </div>
+          <div className="goldGradient flex h-12 w-12 items-center justify-center rounded-full">
+            <img src={mail} className=" h-4 w-auto" />
+          </div>
+        </div>
+        <div className="col-span-1 row-span-2 my-6">
+          <h1 className="textGoldGradient text-sm">¿QUIÉNES SOMOS?</h1>
+          <p className="py-4 text-xs font-[200] leading-5 tracking-widest">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales ex mattis tellus mollis, eu vehicula
-            tortor sollicitudin.
+            tortor sollicitudin. consectetur adipiscing elit. Nunc sodales ex mattis tellus mollis, eu vehicula tortor
+            sollicitudin.
           </p>
-          <h1 className="textGoldGradient underline decoration-gold hover:animate-pulse hover:cursor-pointer">
+          <h1 className="textGoldGradient text-sm underline decoration-gold transition hover:cursor-pointer  hover:opacity-[0.5]">
             LEER MÁS
           </h1>
-        </section>
-        <section className="footerSections">
-          <h1 className="textGoldGradient">ENLACES</h1>
-          <div className="py-2">
-            <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
-              <div className="mr-2 h-1 w-1 rounded-full bg-gold"></div>Mi cuenta / Registrarme
-            </p>
-            <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
-              <div className="mr-2 h-1 w-1 rounded-full bg-gold"></div>Editar info de mi cuenta
-            </p>
-            <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
-              <div className="mr-2 h-1 w-1 rounded-full bg-gold"></div>Cambiar mi contraseña
-            </p>
-            <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
-              <div className="mr-2 h-1 w-1 rounded-full bg-gold"></div>Historial de pedidos
-            </p>
-            <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
-              <div className="mr-2 h-1 w-1 rounded-full bg-gold"></div>Seguimiento de envios
-            </p>
-          </div>
-        </section>
-        <section className="footerSections">
-          <div className="footerNetworksIcons hover:anim  ate-pulse h-12 w-12 hover:cursor-pointer">
-            <img src={arrowUp} className="h-8" />
-          </div>
-        </section>
+        </div>
+        <div className="col-span-1 row-span-2 my-8 flex h-full w-full flex-col justify-around">
+          <h1 className="textGoldGradient mb-4">ENLACES</h1>
+          <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
+            Mi cuenta / Registrarme
+          </p>
+          <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
+            Editar info de mi cuent
+          </p>
+          <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
+            Cambiar mi contraseña
+          </p>
+          <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
+            Historial de pedidos
+          </p>
+          <p className="flex items-center pb-2 decoration-gold transition hover:cursor-pointer hover:underline">
+            Seguimiento de envios
+          </p>
+        </div>
+        <img src={arrowUp} className="goldGradient  row-start-1 h-12 rounded-full " />
       </section>
-      <section className="goldGradient flex h-10  w-full items-center justify-center">
-        <h1 className="font-secondary text-xs">© Copyright 2023 HydroTek - Todos los Derechos Reservados</h1>
-      </section>
+
+      {/*
+    //! No se que esta pasando que se me pone el fondo por arriba
+<div className="!z-30 w-full bg-gold py-2 text-center font-secondary text-xs">
+        © Copyright 2023 HydroTek - <br /> Todos los Derechos Reservados
+      </div> */}
     </footer>
   );
 }
