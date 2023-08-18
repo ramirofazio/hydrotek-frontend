b2e0b58ab4ee228789de46f083bab6c757e43b47
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-17 22:15:55 -0300

    fix ModalNav responsivness

--- src/components/ModalNav.jsx
+++ src/components/ModalNav.jsx
@@ -1,35 +1,42 @@
-import { Popover, Transition } from "@headlessui/react";
+import { Popover } from "@headlessui/react";
 import userProfile from "../assets/userProfile.png";
 import shoppingCart from "../assets/shoppingCart.png";
 import { links } from "./Navbar.jsx";
 import { NavLink, Link } from "react-router-dom";
-import {Bars3BottomRightIcon, XMarkIcon} from "@heroicons/react/24/outline";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 export function ModalNav() {
   return (
     <Popover className="relative">
       <Popover.Button>
-        <Bars3BottomRightIcon className="h-6 w-6" aria-hidden="true"/>
+        <Bars3BottomRightIcon className="h-6 w-6" aria-hidden="true" />
       </Popover.Button>
-      <Popover.Panel className="border-2 bg-black py-5 px-3">
-        <Popover.Button>
-          <XMarkIcon className="h-5 w-6"/>
+      <Popover.Panel className="flex w-fit  flex-col gap-6 rounded-sm border-2 border-black/40 bg-black px-3 py-5 text-base shadow-2xl">
+        <Popover.Button className="goldGradient mb-3 w-fit place-self-end justify-self-center rounded-sm px-[1px] hover:bg-opacity-70">
+          <XMarkIcon className="goldGradient h-5 w-5 stroke-black stroke-[3.5px] hover:stroke-[3.8px]" />
         </Popover.Button>
-        <nav className="text-white">
+        <nav className="flex flex-col gap-4 pl-1 text-white w-[95%]">
           {links.map((l, i) => (
             <ul key={i}>
-              <NavLink to={l.path}>{l.name}</NavLink>
+              <NavLink
+                to={l.path}
+                className={({ isActive, isPending }) =>
+                  isActive ? "border-b-[1px] " : isPending ? "" : ""
+                }
+              >
+                {l.name}
+              </NavLink>
             </ul>
           ))}
-          <section>
-            <Link>
-              <img src={userProfile} className="h-5" />
-            </Link>
-            <Link>
-              <img src={shoppingCart} className="h-5" />
-            </Link>
-          </section>
         </nav>
+        <section className="flex justify-around">
+          <Link>
+            <img src={userProfile} className="h-5 w-5" />
+          </Link>
+          <Link>
+            <img src={shoppingCart} className="h-5 w-5" />
+          </Link>
+        </section>
       </Popover.Panel>
     </Popover>
   );
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,11 +1,12 @@
 import { Pagination } from "../../components/Pagination.jsx";
+import { ModalNav } from "../../components/ModalNav.jsx";
 
 export default function Products() {
   return (
     <div className="outletPrimaryContainer">
       <section className="flex h-[100vh] w-full items-center justify-center">
         <Pagination nButtons={5} currentPage={1} setPage={(p) => console.log(p)} />
-        <h1>Hola soy el Home!</h1>
+        <ModalNav/>
       </section>
     </div>
   );
