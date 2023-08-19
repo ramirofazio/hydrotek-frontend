e2eb025bcbe859da0b10afe4be078d878d624a95
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-18 21:42:36 -0300

    last one

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -4,7 +4,7 @@ export default function Landing() {
   return (
     <div className="outletPrimaryContainer">
       <section className="flex h-[50vh] w-full items-center justify-center">
-        CONTENT
+        <h1>CONTENT</h1>
       </section>
     </div>
   );
