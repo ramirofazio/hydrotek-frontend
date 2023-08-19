46709f70fd92010542bb2536b39393e7aca71dc8
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-18 21:41:57 -0300

    clean

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,12 +1,10 @@
-import { ModalNav } from "../../components/ModalNav";
-import { Categories } from "../../components/Categories";
+
 
 export default function Landing() {
   return (
     <div className="outletPrimaryContainer">
       <section className="flex h-[50vh] w-full items-center justify-center">
-        <ModalNav/>
-        <Categories/>
+        CONTENT
       </section>
     </div>
   );
