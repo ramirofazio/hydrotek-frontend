81ba928b4d87401c4dc8d36fee5d3726c8467997
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-23 00:53:20 -0300

    fix z-index

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -7,7 +7,7 @@ export default function InfoCard({ text }) {
     <div className="mx-auto border-blue mb-3 md:mb-10">
       <div className="relative mx-auto w-[70%] sm:w-[60%]">
         <img src={infoCardBorder} className="" />
-        <div className="absolute inset-1 top-3 border-2 p-3 text-center xs:top-8 xs:text-xl s:top-10 md:flex md:flex-col md:gap-4">
+        <div className="absolute z-30 inset-1 top-3 border-2 p-3 text-center xs:top-8 xs:text-xl s:top-10 md:flex md:flex-col md:gap-4">
           <h1 className="h-fit text-lg uppercase lg:text-2xl xs:text-2xl s:p-2 s:text-3xl sm:text-4xl">
             {text || "Cultivo sin tierra, crecimiento sin límites"}
           </h1>
