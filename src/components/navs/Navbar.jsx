30be428a1bdcebfa74b0f7e63bd13311233329e4
Ramiro <ramifazio@gmail.com>
2023-08-24 21:11:23 -0300

    Update navBar logo width

--- src/components/navs/Navbar.jsx
+++ src/components/navs/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
     <nav className="flex w-full items-center justify-between p-8 2xl:px-24">
       <NavLink to="/">
         <Atropos innerClassName="rounded-full" shadow={false}>
-          <img src={logo} className="w-20 transition hover:opacity-70" />
+          <img src={logo} className="w-14 transition hover:opacity-70 xl:w-20" />
         </Atropos>
       </NavLink>
       <ModalNav />
