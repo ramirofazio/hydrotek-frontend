4f7772b777f8c9b456e0c20d7cfe8928ce75eb45
Ramiro <ramifazio@gmail.com>
2023-08-24 22:04:29 -0300

    update button hover

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({ text, classname, ...props }) => {
   return (
     <button
-      className={`rounded-[4.325rem] border-2 border-gold bg-transparent px-6 py-2 text-white hover:bg-black/30 hover:text-white/70 ${classname}`}
+      className={`rounded-[4.325rem] border-2 border-gold bg-transparent px-6 py-2 uppercase text-white transition hover:bg-gold hover:text-[#1B142C] ${classname}`}
       {...props}
     >
       {text}
