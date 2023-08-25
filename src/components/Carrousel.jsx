c8248cc3c80c84898f02e77f03f0d7e94a406adf
Ramiro <ramifazio@gmail.com>
2023-08-25 08:34:46 -0300

    Carrousel and productCard details

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -23,7 +23,7 @@ export function Carrousel({ content }) {
       loop={true}
       autoplay={{
         delay: 2500,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
         pauseOnMouseEnter: true,
       }}
       pagination={pagination}
@@ -33,17 +33,13 @@ export function Carrousel({ content }) {
       modules={[Pagination, Autoplay]}
       className="mb-10 w-full"
       breakpoints={{
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 40,
-        },
         1024: {
-          slidesPerView: 3,
-          spaceBetween: 20,
+          slidesPerView: 2,
+          spaceBetween: 100,
         },
         1280: {
-          slidesPerView: 4,
-          spaceBetween: 70,
+          slidesPerView: 3,
+          spaceBetween: 250,
         },
       }}
     >
@@ -53,7 +49,7 @@ export function Carrousel({ content }) {
             <div
               className={`${
                 isActive ? "mt-5 opacity-100" : "pointer-events-none opacity-30 blur-md"
-              } grid h-full w-full place-items-center p-2 transition`}
+              } grid h-full w-full place-items-center p-2 transition md:w-[50%] lg:w-[80%] xl:w-full`}
             >
               {component}
             </div>
