4b58965b6ccdc6bca5a0bb13fa39bd7e312f357d
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-25 16:07:03 -0300

    ready basic SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,28 +3,41 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
+const mockProducts = ["sistema x", "fertilizante a", "plug 0-34", "enraizador3"];
+
 export function SearchBar() {
   const { t } = useTranslation();
-  const mockProducts = ["sistema x", "fertilizante a", "plug 0-34"];
-  const query = useState("")
+  const [query, setQuery] = useState("");
+
+  // ? Basica logica de filtrado de la searchbar, disponible a cambios.
+  // Falta la logica de bsuqueda/filtrado en el onClick del Combobox.Button
+
+  const filteredProducts =
+    query === ""
+      ? mockProducts
+      : mockProducts.filter((person) => {
+        return person.toLowerCase().includes(query.toLowerCase());
+      });
 
   return (
     <div className="w-fit border-[1px] border-gold">
       <Combobox>
-        <div className="relative flex w-fit place-items-center">
+        <div className="relative flex w-fit place-items-center ">
           <Combobox.Input
+            className="ml-1 bg-transparent p-1 text-white focus:outline-none"
+            onChange={(event) => setQuery(event.target.value)}
             placeholder={t("common.search")}
-            className="ml-1 bg-transparent p-1 text-white focus:border-0 focus:outline-0"
           />
           <Combobox.Button className="goldGradient rounded-sm">
             <MagnifyingGlassIcon className="m-1 h-6 w-6 text-white" />
           </Combobox.Button>
         </div>
-
-        <Combobox.Options className="absolute  border-2">
-          {mockProducts.map((p, i) => {
-            <Combobox.Option key={i}><p>HOLAAA</p></Combobox.Option>;
-          })}
+        <Combobox.Options className="border-2 border-black/40 bg-black/80 absolute shadow-2xl mt-2">
+          {filteredProducts.map((product, i) => (
+            <Combobox.Option key={i} value={product} className="my-4 mx-3">
+              <h1 className="text-sm">{product}</h1>
+            </Combobox.Option>
+          ))}
         </Combobox.Options>
       </Combobox>
     </div>
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,10 +1,9 @@
-import { SearchBar } from "src/components/searchBar";
+
 
 export default function Products() {
   return (
     <div className="content">
       <section className="ml-10 ">
-        <SearchBar />
       </section>
     </div>
   );
