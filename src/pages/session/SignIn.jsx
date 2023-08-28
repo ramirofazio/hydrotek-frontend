918d96df00dd8029295b762486fc98c7172b1ab1
Ramiro <ramifazio@gmail.com>
2023-08-28 14:42:31 -0300

    Add module export on session

--- src/pages/session/SignIn.jsx
+++ src/pages/session/SignIn.jsx
@@ -1,6 +1,6 @@
 import loginBorder from "assets/loginBorder.png";
 
-export default function SignIn() {
+export function SignIn() {
   return (
     <div className="content grid place-items-center p-8">
       <img src={loginBorder} className="" />
