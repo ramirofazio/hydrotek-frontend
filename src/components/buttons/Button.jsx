75d6dfec585e2e48e03cb902ac4436e9925a7586
Ramiro <ramifazio@gmail.com>
2023-08-28 19:38:04 -0300

    update tracking

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({ text, img, imgClassname, classname, ...props }) => {
   return (
     <button
-      className={`rounded-[4.325rem] border-2 border-gold bg-transparent px-6 py-2 uppercase text-white transition hover:bg-gold hover:text-[#1B142C] ${classname}`}
+      className={`rounded-[4.325rem] border-2 border-gold bg-transparent px-6 py-2 uppercase tracking-widest text-white transition hover:bg-gold hover:text-[#1B142C] ${classname}`}
       {...props}
     >
       {img && <img src={img[0]} className={img[1]} />}
