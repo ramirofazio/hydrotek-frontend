f24f83382e2646b5cc51216b4a7637e6b4290bca
Ramiro <ramifazio@gmail.com>
2023-08-28 15:11:32 -0300

    Login modal test, waiting imgs

--- src/components/navs/Navbar.jsx
+++ src/components/navs/Navbar.jsx
@@ -1,4 +1,5 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import logo from "assets/blackLogo.png";
 import userProfile from "assets/userProfile.png";
 import shoppingCart from "assets/shoppingCart.png";
@@ -6,9 +7,14 @@ import { Categories } from "./Categories";
 import Atropos from "atropos/react";
 import { ModalNav } from "./ModalNav";
 import { links } from "src/utils";
+import { SignIn } from "pages/session/index";
 
 export const Navbar = () => {
-  const navigate = useNavigate();
+  const [loginShow, setLoginShow] = useState(false);
+
+  const alternModal = () => {
+    setLoginShow(!loginShow);
+  };
 
   return (
     <nav className="flex w-full items-center justify-between p-8 2xl:px-24">
@@ -41,9 +47,10 @@ export const Navbar = () => {
         ))}
       </ul>
       <section className="hidden w-24 justify-evenly lg:flex">
-        <img src={userProfile} className="icons" onClick={() => navigate("/signIn")} />
+        <img src={userProfile} className="icons" onClick={alternModal} />
         <img src={shoppingCart} className="icons" />
       </section>
+      <SignIn isOpen={loginShow} alternModal={alternModal} />
     </nav>
   );
 };
--- src/pages/session/SignIn.jsx
+++ src/pages/session/SignIn.jsx
@@ -1,9 +1,43 @@
 import loginBorder from "assets/loginBorder.png";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
-export function SignIn() {
+export function SignIn({ isOpen, alternModal }) {
   return (
-    <div className="content grid place-items-center p-8">
-      <img src={loginBorder} className="" />
-    </div>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-30" onClose={alternModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-90" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="grid h-full w-full place-items-center p-2">
+                <div className="relative h-full max-w-fit  bg-base">
+                  <img src={loginBorder} className="" />
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
