84983664f8569adf5999bc96faf329d46755dd5f
Ramiro <ramifazio@gmail.com>
2023-08-30 20:45:28 -0300

    Cloudinary icons connect

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,8 @@ export const icons = {
   mail: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325742/HYD/icons/mail.png",
   instagram: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325742/HYD/icons/instagram.png",
   wpp: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325743/HYD/icons/whatsapp.png",
+  apple: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325743/HYD/icons/apple.png",
+  google: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325743/HYD/icons/google.png",
 };
 
 export const logos = {
@@ -13,13 +15,15 @@ export const logos = {
 
 export const backgrounds = {
   productBorder: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/productBorder.png",
+  borderTop: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/borderTop.png",
+  borderBottom: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/borderBottom.png",
   productBorderGradient:
     "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/productBorderGradient.png",
   blueGradient: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/blueGradient.png",
   circuit: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/circuit.png",
   goldCircuit: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325909/HYD/others/goldCircuit.png",
-  techDots: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325911/HYD/others/techDots.png"
+  techDots: "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325911/HYD/others/techDots.png",
 };
 
-export const defaultProduct = "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325911/HYD/others/defaultProduct.png";
-
+export const defaultProduct =
+  "https://res.cloudinary.com/djdtbqhxm/image/upload/v1693325911/HYD/others/defaultProduct.png";
--- src/pages/session/SignIn.jsx
+++ src/pages/session/SignIn.jsx
@@ -1,13 +1,9 @@
-import blackLogo from "assets/blackLogo.png";
-import facebook from "assets/facebook.png";
-import apple from "assets/apple.png";
-import google from "assets/google.png";
 import { Button } from "src/components/buttons";
+import { backgrounds, icons } from "src/assets";
 
 export function SignIn() {
   return (
     <div className="grid h-screen w-full place-items-center gap-4 p-4  xl:w-[30%]">
-      <img src={blackLogo} className="w-14" />
       <form>
         <input type="text" onChange={"#"} placeholder="EMAIL/NOMBRE DE USUARIO" />
         <input type="password" onChange={"#"} placeholder="CONTRASEÑA" />
@@ -16,17 +12,17 @@ export function SignIn() {
       <div className="w-full rounded-full border-[1px] border-dashed border-white/60" />
       <div className="grid place-items-center">
         <Button
-          img={[google, "icons mr-6 "]}
+          img={[icons.google, "icons mr-6 "]}
           text={`INICIA SESIÓN CON GOOGLE`}
           classname={"w-[100%] py-0 flex  items-center text-[10px]"}
         />
         <Button
-          img={[apple, "icons mr-6 "]}
+          img={[icons.apple, "icons mr-6 "]}
           text={`INICIA SESIÓN CON APPLE`}
           classname={"w-[100%] py-0 flex  items-center text-[10px]"}
         />
         <Button
-          img={[facebook, "icons mr-6 "]}
+          img={[icons.facebook, "icons mr-6 "]}
           text={`INICIA SESIÓN CON FACEBOOK`}
           classname={"w-[100%] py-0 flex  items-center text-[10px]"}
         />
