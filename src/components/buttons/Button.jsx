e1d396f294e5d788ec433e9e7d2d6e77c688709f
Ramiro <ramifazio@gmail.com>
2023-08-30 21:42:53 -0300

    Update button to match images and hidden p

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,11 +1,11 @@
-export const Button = ({ text, img, classname, ...props }) => {
+export const Button = ({ text, img, classname, pClassname, ...props }) => {
   return (
     <button
-      className={`rounded-[4.325rem] border-2 border-gold bg-transparent px-6 py-2 uppercase tracking-widest text-white transition hover:bg-gold hover:text-[#1B142C] ${classname}`}
+      className={`rounded-full border-2 border-gold bg-transparent px-6 py-2 uppercase tracking-widest text-white transition hover:bg-gold hover:text-[#1B142C] ${classname}`}
       {...props}
     >
       {img && <img src={img[0]} className={img[1]} />}
-      {text}
+      <p className={`${pClassname}`}>{text}</p>
     </button>
   );
 };
