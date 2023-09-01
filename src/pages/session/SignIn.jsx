9aeb44876dfd3a42eb5b7f26df7a8431c162cd4b
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-31 23:43:35 -0300

    fix responsive

--- src/pages/session/SignIn.jsx
+++ src/pages/session/SignIn.jsx
@@ -38,13 +38,10 @@ export function SignIn() {
   };
 
   return (
-    <main className="relative mx-4 mb-10 grid place-items-center gap-6 py-10 sm:mx-auto sm:w-[60%] xl:w-[40%] xl:py-20">
-      <img src={backgrounds.borderBgFlip} className="absolute -z-10 hidden w-full xl:inline" />
-      <section className="xl:w-[90%]">
-        <img src={backgrounds.borderTop} className="xl:hidden" />
-        <h1 className="-mt-20 mb-14 text-center lg:-mt-32 lg:text-3xl xl:mb-20 xl:mt-14 xl:text-4xl">
-          {t("common.logIn")}
-        </h1>
+    <main className="relative mx-4  mb-14 grid place-items-center gap-6 py-10 sm:mx-auto sm:w-[60%] md:my-[7rem] xl:w-[40%] xl:py-20 ">
+      <section className="xl:w-[90%] ">
+        <img src={backgrounds.borderTop} className="xl:absolute xl:inset-x-0 xl:top-0" />
+        <h1 className=" -mt-20 mb-14 text-center lg:-mt-32 lg:text-3xl xl:mt-14 xl:text-4xl">{t("common.logIn")}</h1>
         <form
           className="grid place-items-center gap-4 px-6 lg:mx-auto lg:w-[80%] xl:w-full xl:gap-6"
           onSubmit={handleSubmit}
@@ -72,7 +69,7 @@ export function SignIn() {
         </form>
       </section>
       <div className="w-[80%] border-[1px] border-dashed border-white/40" />
-      <section className="flex w-full justify-around p-6 lg:w-[80%] lg:flex-col lg:gap-4 xl:w-[60%] xl:gap-6">
+      <section className="flex w-full justify-around p-6 lg:w-[80%] lg:flex-col lg:gap-4 xl:w-[60%] xl:gap-6 ">
         {authBtns.map(({ socialNetwork, icon }, index) => (
           <Button
             key={index}
@@ -84,7 +81,7 @@ export function SignIn() {
           />
         ))}
       </section>
-      <section className="my-2 grid w-full gap-6 px-6 text-center lg:my-6 lg:w-[80%] lg:gap-2">
+      <section className="my-2 grid w-full gap-6 px-6 text-center lg:my-6 lg:w-[80%] lg:gap-2 ">
         <p>
           {t("common.dontHaveAccount")}
           <br className="lg:hidden" />
@@ -100,7 +97,7 @@ export function SignIn() {
           </strong>
         </p>
       </section>
-      <img src={backgrounds.borderBottom} className="absolute bottom-0 -z-10 xl:hidden" />
+      <img src={backgrounds.borderBottom} className="absolute bottom-0 -z-10" />
     </main>
   );
 }
