685058bff5c6bb549053a6bc09572312b4ad742b
Ramiro <ramifazio@gmail.com>
2023-09-07 23:10:17 -0300

    Update star icons

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,10 @@
-import { StarIcon } from "@heroicons/react/24/solid";
-
 export function Rating({ value = 2 }) {
   const stars = [1, 2, 3, 4, 5];
 
   return (
     <div className="flex w-fit gap-1">
       {stars.map((s, i) => (
-        <StarIcon
-          key={i}
-          className={`h-6 w-6 ${s <= value ? "stroke-none text-gold" : "stroke-gold text-transparent"}`}
-        />
+        <i key={i} className={`${s <= value ? "ri-star-fill" : "ri-star-line"} text-gold`} />
       ))}
     </div>
   );
