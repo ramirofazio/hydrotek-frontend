c2a547be6271abc30a085e16d8dcf94dedd1665b
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-09-14 19:43:40 -0300

    overflow

--- src/components/cards/CartArticleCard.jsx
+++ src/components/cards/CartArticleCard.jsx
@@ -1,7 +1,6 @@
 import { products, backgrounds } from "assets";
 import { Quantity } from "..";
 
-
 export function CartArticleCard({ img, name, price }) {
   console.log(products);
 
@@ -12,9 +11,9 @@ export function CartArticleCard({ img, name, price }) {
         <img className="absolute -z-10 h-[105%] w-[79%]" src={backgrounds.cartProductGradient} alt="" />
         <img className="max-w-[80%]" src={img || products.defaultOne} alt="" />
       </picture>
-      <article className="flex flex-col justify-center  gap-1 border-2">
-        <h1 className="overflow-hidden text-lg">{name || "fertilizante"}</h1> {/* //* resolver overflow */}
-        <h2 className=" textGoldGradient ">{price || "$99.99"}</h2>
+      <article id="problem" className="break-all flex flex-col break-normal justify-center gap-1 border-2">
+        <h1 className="text-lg">{name || "fertilizante"}</h1> {/* //* resolver overflow */}
+        <h2 className="textGoldGradient ">{price || "$99.99"}</h2>
       </article>
       <article className="col-span-2 mt-4 ">
         <Quantity />
--- src/pages/shoppingCart/ShoppingCart.jsx
+++ src/pages/shoppingCart/ShoppingCart.jsx
@@ -6,7 +6,7 @@ export default function ShoppingCart({ subtotal, totalPrice, deliveryPrice }) {
   const { t } = useTranslation();
   const arr = [1, 2, 3];
   return (
-    <main className="content mx-auto mb-14 mt-5 flex w-[92%] flex-col ">
+    <main className="content mx-auto mb-[6rem] mt-5  flex w-[92%] flex-col ">
       <div className=" border-b-2 border-gold p-4">
         <h1 className="mx-auto w-fit">{t("shopping-cart.your-cart")}</h1>
       </div>
@@ -25,9 +25,9 @@ export default function ShoppingCart({ subtotal, totalPrice, deliveryPrice }) {
             <h1 className="text-[0.95rem]">{t("common.add")}</h1>
           </button>
         </div>
-        <div className="px-5 py-8 mx-auto mt-10 w-[95%] rounded-lg border-2 border-gold bg-black">
-          <h1 className="my-5 mx-auto w-fit">{t("order.order-data")}</h1>
-          <div className="flex flex-col gap-4">
+        <div className="mx-auto my-10 w-[90%] rounded-lg border-2 border-gold bg-black px-5 py-8">
+          <h1 className="mx-auto my-5 w-fit">{t("order.order-data")}</h1>
+          <div className="flex flex-col gap-5  ">
             <div>
               <h1>{t("order.subtotal")}</h1>
               <h2 className=" textGoldGradient ">{subtotal || "$99.99"}</h2>
@@ -41,7 +41,18 @@ export default function ShoppingCart({ subtotal, totalPrice, deliveryPrice }) {
               <h2 className=" textGoldGradient ">{totalPrice || "$99.99"}</h2>
             </div>
           </div>
+
+          <button className="hidden lg:inline rounded-2xl bg-gold-gradient px-8 py-2">
+            <h1 className="text-lg">{t("order.pay-order")}</h1>
+          </button>
         </div>
+
+        <div className="lg:hidden w-fit mx-auto">
+          <button className="inline mx-auto rounded-2xl bg-gold-gradient px-8 py-2">
+            <h1 className="w-fit text-lg">{t("order.pay-order")}</h1>
+          </button>
+        </div>
+
       </section>
     </main>
   );
