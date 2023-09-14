fc25265792df7c624245755b9a99e17c4498bc4e
Ramiro <ramifazio@gmail.com>
2023-09-13 23:58:33 -0300

    Fix and finish loader y details on Auth3Btn

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,15 +3,15 @@ import { logos } from "assets/index";
 export function Loader({ className }) {
   useEffect(() => {
     //? Saca scroll mientras se monta
-    document.body.classList.add("overflow-hidden");
+    document.body.classList.add("pointer-events-none", "overflow-hidden");
     return () => {
-      document.body.classList.remove("overflow-hidden");
+      document.body.classList.remove("pointer-events-none", "overflow-hidden");
     };
   }, []);
 
   return (
     <main
-      className={`absolute z-40 grid h-full w-full  place-content-center place-items-center bg-black/70 ${className}`}
+      className={`absolute z-50 grid aspect-square  h-20 place-content-center place-items-center rounded-md bg-black/70 lg:h-40 ${className}`}
     >
       {/* <img src={logos.hydBlack} className="animate-pulse" />  //?OTRA OPCION */}
       <i className="ri-loader-2-fill animate-spin text-6xl text-gold" />
