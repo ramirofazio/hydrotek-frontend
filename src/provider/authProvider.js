3dcf6badd859e34adab8df62d9730bf2231e541a
Ramiro <ramifazio@gmail.com>
2023-09-14 22:11:41 -0300

    Add authentication context

---
+++ src/provider/authProvider.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { getOfStorage, saveInStorage, deleteOfStorage } from "utils/localStorage";
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [token, setToken_] = useState(getOfStorage("accessToken"));
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      saveInStorage("accessToken", token);
+    } else {
+      delete axios.defaults.headers.common["Authorization"];
+      deleteOfStorage("accessToken");
+    }
+  }, [token]);
+
+  const setToken = (newToken) => {
+    setToken_(newToken);
+  };
+
+  const contextValue = useMemo(() => ({ token, setToken }), [token]);
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
