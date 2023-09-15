d81c4cdf4da840eea4c4cb4ebeb10763d34c3d92
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-09-15 12:38:15 -0300

    finissh component stylyng & responsive

--- src/components/cards/CartArticleCard.jsx
+++ src/components/cards/CartArticleCard.jsx
@@ -5,17 +5,20 @@ export function CartArticleCard({ img, name, price }) {
   console.log(products);
 
   return (
-    <main className="grid grid-cols-2 gap-2 border-b-[1px] border-gold py-8">
-      <picture className="relative flex items-center justify-center">
+    <main className="grid grid-cols-2 gap-2 border-b-[1px] border-gold py-8 sm:grid-cols-3  sm:gap-0 lg:max-w-[75%] lg:gap-8">
+      <picture className=" relative flex items-center justify-center  sm:max-w-[200px]">
         <img className="absolute h-[110%] w-[80%]" src={backgrounds.cartProductBorder} />
         <img className="absolute -z-10 h-[105%] w-[79%]" src={backgrounds.cartProductGradient} alt="" />
         <img className="max-w-[80%]" src={img || products.defaultOne} alt="" />
       </picture>
-      <article id="problem" className="break-all flex flex-col break-normal justify-center gap-1 border-2">
-        <h1 className="text-lg">{name || "fertilizante"}</h1> {/* //* resolver overflow */}
-        <h2 className="textGoldGradient ">{price || "$99.99"}</h2>
+      <article className="flex flex-col justify-center gap-1 break-normal break-all  lg:col-span-2">
+        <h1 className="text-lg">{name || "fertilizanteeeeeeee superx"}</h1> {/* //* resolver overflow */}
+        <h2 className=" textGoldGradient ">{price || "$99.99"}</h2>
+        <article className="col-span-2 mt-4 hidden w-fit sm:inline ">
+          <Quantity />
+        </article>
       </article>
-      <article className="col-span-2 mt-4 ">
+      <article className="col-span-2 mt-8 sm:hidden ">
         <Quantity />
       </article>
     </main>
--- src/pages/shoppingCart/ShoppingCart.jsx
+++ src/pages/shoppingCart/ShoppingCart.jsx
@@ -7,52 +7,51 @@ export default function ShoppingCart({ subtotal, totalPrice, deliveryPrice }) {
   const arr = [1, 2, 3];
   return (
     <main className="content mx-auto mb-[6rem] mt-5  flex w-[92%] flex-col ">
-      <div className=" border-b-2 border-gold p-4">
+      <header className=" border-b-2 border-gold p-4">
         <h1 className="mx-auto w-fit">{t("shopping-cart.your-cart")}</h1>
-      </div>
-      <section className="grid place-items-center gap-10 ">
+      </header>
+      <section className="grid place-items-center gap-10 lg:place-items-start ">
         {arr.map((a, i) => (
           <CartArticleCard key={i} />
         ))}
       </section>
-      <section className="mt-10">
-        <div className="flex flex-col place-items-center gap-4">
+      <section className=" mt-10 lg:grid lg:grid-cols-5 lg:items-center">
+        <article className="flex  flex-col place-items-center gap-4 lg:col-span-2">
           <h1 className="mx-auto w-fit text-lg ">{t("shopping-cart.promotional-code")}</h1>
-          <div className=" w-[75%]">
-            <Input type="text" placeholder="codigo" className="!p-1 !text-lg uppercase" />
+          <div className="w-[75%] lg:w-full">
+            <Input type="text" placeholder="codigo" className="!p-1 lg:!pl-6 !text-lg uppercase " />
           </div>
           <button className="rounded-2xl bg-gold-gradient px-8 py-1">
             <h1 className="text-[0.95rem]">{t("common.add")}</h1>
           </button>
-        </div>
-        <div className="mx-auto my-10 w-[90%] rounded-lg border-2 border-gold bg-black px-5 py-8">
-          <h1 className="mx-auto my-5 w-fit">{t("order.order-data")}</h1>
+        </article>
+        <article className="mx-auto my-10 w-[90%] rounded-lg border-2 border-gold bg-black px-5 py-8 md:px-[6rem] lg:col-span-3">
+          <h1 className="mx-auto my-5 w-fit md:mx-0">{t("order.order-data")}</h1>
           <div className="flex flex-col gap-5  ">
-            <div>
+            <div className="md:flex  md:justify-between md:border-b-[1px] md:border-dashed md:border-gold">
               <h1>{t("order.subtotal")}</h1>
               <h2 className=" textGoldGradient ">{subtotal || "$99.99"}</h2>
             </div>
-            <div>
+            <div className="md:flex  md:justify-between md:border-b-[1px] md:border-dashed md:border-gold">
               <h1>{t("order.delivery")}</h1>
               <h2 className=" textGoldGradient ">{deliveryPrice || "$99.99"}</h2>
             </div>
-            <div>
+            <div className="md:flex  md:justify-between md:border-b-[1px] md:border-dashed md:border-gold">
               <h1>{t("order.total-price")}</h1>
               <h2 className=" textGoldGradient ">{totalPrice || "$99.99"}</h2>
             </div>
           </div>
 
-          <button className="hidden lg:inline rounded-2xl bg-gold-gradient px-8 py-2">
+          <button className="mt-10 hidden rounded-2xl bg-gold-gradient px-8 py-2 lg:inline">
             <h1 className="text-lg">{t("order.pay-order")}</h1>
           </button>
-        </div>
+        </article>
 
-        <div className="lg:hidden w-fit mx-auto">
-          <button className="inline mx-auto rounded-2xl bg-gold-gradient px-8 py-2">
+        <div className="mx-auto w-fit lg:hidden">
+          <button className="mx-auto inline rounded-2xl bg-gold-gradient px-8 py-2">
             <h1 className="w-fit text-lg">{t("order.pay-order")}</h1>
           </button>
         </div>
-
       </section>
     </main>
   );
