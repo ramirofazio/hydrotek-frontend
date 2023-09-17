52924f1756d637e85a37b6724b6750c8a3f79e93
Ramiro <ramifazio@gmail.com>
2023-09-16 21:12:13 -0300

    Primary logic to render subSections on Profile

---
+++ src/pages/user/MyData.jsx
@@ -0,0 +1,3 @@
+export function MyData() {
+  return(<main>My Data</main>);
+}
--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,10 +1,18 @@
 import { borders } from "src/assets";
-import { Header, ButtonList } from "./";
+import { Header, ButtonList, MyBuys, MyData, MySavedPosts } from "./";
 import { Avatar } from "src/components/user";
 import { useState } from "react";
 
+const componentMapping = {
+  MyData: <MyData />,
+  MyBuys: <MyBuys />,
+  MySavedPosts: <MySavedPosts />,
+};
+
 export function Profile() {
   const [selectedBtn, setSelectedBtn] = useState("MyData");
+
+  const selectedSection = componentMapping[selectedBtn] || <MyData />;
 
   return (
     <main className="grid gap-10">
@@ -15,6 +23,7 @@ export function Profile() {
         <img src={borders.profile} className="absolute bottom-0 right-5 w-48 rotate-180 animate-pulse" />
       </section>
       <ButtonList selectedBtn={selectedBtn} setSelectedBtn={setSelectedBtn} />
+      {selectedSection}
     </main>
   );
 }
