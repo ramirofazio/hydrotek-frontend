2886d39e546fd2b0ebe3e63d70957ce3ebe20920
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-10-02 12:38:58 -0300

    add redirect

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,18 @@
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigate } from "react-router-dom";
 import { Navbar } from "src/components";
 import { useSelector, useDispatch } from "react-redux";
 import { actionsUser, actionsShoppingCart } from "src/redux/reducers";
+import { useEffect } from "react";
 
 export function ProtectedRoute() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userInfo = useLoaderData();
   const { user } = useSelector((state) => state);
+
+  useEffect(() => {
+    if (!userInfo.userInfo.accessToken) navigate("/session/signIn");
+  }, []);
 
   /* useEffect(() => {
     console.log(userInfo)
