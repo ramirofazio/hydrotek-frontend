3cb89beaaeeb40e09e5d40bfaaa1a27d2f33ff51
Ramiro <ramifazio@gmail.com>
2023-10-05 22:22:09 -0300

    details on modal

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -15,7 +15,7 @@ export function Modal({ isOpen, onClose, children }) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-80" />
           </Transition.Child>
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
@@ -28,7 +28,7 @@ export function Modal({ isOpen, onClose, children }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-base p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-base/90 p-6 text-left align-middle shadow-xl transition-all">
                   {children}
                 </Dialog.Panel>
               </Transition.Child>
