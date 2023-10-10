afcf067a409af39c192b1fc5043a2e0985880703
Ramiro <ramifazio@gmail.com>
2023-10-09 23:09:19 -0300

    final CI details

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiHydro = axios.create({
   baseURL: API_URL,
   headers: {
     // ? Configuracion las cabeceras CORS
-    "Access-Control-Allow-Origin": import.meta.env.VITE_ACCESS_URL || "http://localhost:5173",
+    "Access-Control-Allow-Origin": import.meta.env.VITE_ACCESS_URL,
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Origin, Content-Type, Accept, Authorization",
   },
