fea15d3464c170f87b4758ff7a6a20e09cbb0118
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-10-31 17:44:23 -0300

    show save option

--- src/components/cards/BlogPostCard.jsx
+++ src/components/cards/BlogPostCard.jsx
@@ -2,33 +2,45 @@ import { useTranslation } from "react-i18next";
 import { defaultPost, borders } from "src/assets";
 import { Link } from "react-router-dom";
 
-export function BlogPostCard({ id, date, showPreview = true, title, text, img = defaultPost, saved, setSavedPosts }) {
+export function BlogPostCard({
+  id,
+  date,
+  showPreview = true,
+  showSave = true,
+  title,
+  text,
+  img = defaultPost,
+  saved,
+  setSavedPosts,
+}) {
   const { t } = useTranslation();
   const textPreview = text?.slice(0, 100);
 
   return (
     <main className="relative ">
-      <button
-        onClick={() =>
-          setSavedPosts((prev) => {
-            let prevSaves = { ...prev };
-            if (saved) {
-              delete prevSaves[id];
-            } else {
-              prevSaves[id] = id;
-            }
-            console.log("SAVES: ", prevSaves)
-            return prevSaves;
-          })
-        }
-        className="goldGradient absolute left-[93%]  z-30 rounded-full px-1 py-[0.5px] lg:px-1.5 lg:py-1"
-      >
-        {saved ? (
-          <i className="ri-bookmark-fill text-2xl  text-white lg:text-3xl"></i>
-        ) : (
-          <i className="ri-bookmark-line text-2xl text-white lg:text-3xl"></i>
-        )}
-      </button>
+      {showSave && (
+        <button
+          onClick={() =>
+            setSavedPosts((prev) => {
+              let prevSaves = { ...prev };
+              if (saved) {
+                delete prevSaves[id];
+              } else {
+                prevSaves[id] = id;
+              }
+              console.log("SAVES: ", prevSaves);
+              return prevSaves;
+            })
+          }
+          className="goldGradient absolute left-[93%]  z-30 rounded-full px-1 py-[0.5px] lg:px-1.5 lg:py-1"
+        >
+          {saved ? (
+            <i className="ri-bookmark-fill text-2xl  text-white lg:text-3xl"></i>
+          ) : (
+            <i className="ri-bookmark-line text-2xl text-white lg:text-3xl"></i>
+          )}
+        </button>
+      )}
       <picture className="relative flex max-w-[300px] items-center justify-center md:max-w-[350px]">
         <img className="absolute -z-10 max-h-full max-w-full" src={img} alt="post_image" />
         <img className="bottom-1 max-w-full" src={borders.blogPostBorder} alt="post_border" />
