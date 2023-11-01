12809d2a01fcd0a5b5681c39a5bf66ed372b93dc
Your Name <ramifazio@gmail.com>
2023-11-01 15:39:13 -0300

    wip modal v1

---
+++ src/components/modals/WorkInProgressModal.jsx
@@ -0,0 +1,19 @@
+import { Modal } from "..";
+import wip from "assets/wip.gif";
+import { logos } from "src/assets";
+
+export function WorkInProgressModal({ isOpen, onClose }) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <section className="grid place-items-center gap-4 p-2">
+        <h2 className="textGoldGradient font-bold">¡Hemos lanzado nuestra web!</h2>
+        <p className="w-full text-center text-sm">
+          Ciertos módulos aún no están disponibles. Agradecemos tu comprensión mientras trabajamos en mejorar tu
+          experiencia. Permanece atento para futuras actualizaciones.
+        </p>
+        <img src={wip} className="w-52 rounded-md object-cover" />
+        <img src={logos.hydText} className="w-20" />
+      </section>
+    </Modal>
+  );
+}
