8be2ff01603e3f61d89a76a5d073996f2b490b12
Your Name <ramifazio@gmail.com>
2023-11-01 16:07:24 -0300

    add modal to products

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -3,10 +3,15 @@ import { products } from "assets";
 import { Button } from "components/buttons";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { WorkInProgressModal } from "../modals";
 // import { AddToCart } from "src/pages/shoppingCart/AddToCart";
 
 export function ProductCard({ imgUrl, name, price = 200, id, showBtn = true }) {
   const { t } = useTranslation();
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={`grid h-full w-fit p-4 md:w-[100%] xl:p-0`}>
       <Atropos highlight={false} shadow={false} className="h-full w-full cursor-pointer xl:pb-4">
@@ -24,13 +29,29 @@ export function ProductCard({ imgUrl, name, price = 200, id, showBtn = true }) {
         <h2 className="textGoldGradient mb-4 md:mb-2 md:place-self-start">{`${price}`}</h2>
         {showBtn && (
           <div className="flex  items-center gap-3 md:gap-8 md:place-self-start s:gap-5">
-            <Link to={`/productDetail/${id}`} className=" md:place-self-start">
+            {/* <Link to={`/productDetail/${id}`} className="md:place-self-start"> */}
+            <Link className="md:place-self-start" onClick={() => setIsOpen(true)}>
               <Button text={t("common.buy-now")} pClassname={"font-primary"} />
             </Link>
             {/* <AddToCart price={price} productId={id}/> */}
           </div>
         )}
       </div>
+      <WorkInProgressModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        text={
+          <p className="text-center text-sm">
+            Si quieres comprar {name}
+            <br />
+            <a
+              href={`http://wa.me/5491170823697?text=Hola%21%20Vengo%20de%20la%20web.%20Me%20interes%C3%B3%20el%20producto%20${name}`}
+            >
+              <strong>comunicate con nosotros</strong>
+            </a>
+          </p>
+        }
+      />
     </div>
   );
 }
--- src/components/modals/WorkInProgressModal.jsx
+++ src/components/modals/WorkInProgressModal.jsx
@@ -2,15 +2,16 @@ import { Modal } from "..";
 import wip from "assets/wip.gif";
 import { logos } from "src/assets";
 
-export function WorkInProgressModal({ isOpen, onClose }) {
+export function WorkInProgressModal({ isOpen, onClose, text }) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <section className="grid place-items-center gap-4 p-2">
         <h2 className="textGoldGradient font-bold">¡Hemos lanzado nuestra web!</h2>
-        <p className="w-full text-center text-sm">
+        <p className="text-center text-sm">
           Ciertos módulos aún no están disponibles. Agradecemos tu comprensión mientras trabajamos en mejorar tu
           experiencia. Permanece atento para futuras actualizaciones.
         </p>
+        {text}
         <img src={wip} className="w-52 rounded-md object-cover" />
         <img src={logos.hydText} className="w-20" />
       </section>
