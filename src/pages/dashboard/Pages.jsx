c0b73c2544e24c5688467eeb2c0e46540b4cc37f
Ramiro <ramifazio@gmail.com>
2023-11-14 20:20:04 -0300

    yes

--- src/pages/dashboard/Pages.jsx
+++ src/pages/dashboard/Pages.jsx
@@ -1,3 +1,8 @@
 export function Pages({ selectedComponent }) {
   return <main className="flex w-full flex-col items-center gap-4 overflow-scroll">{selectedComponent}</main>;
 }
+
+/**
+
+    Yes?
+ */
