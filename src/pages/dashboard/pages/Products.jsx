ad66041166f1f98b69d1d0cf334f930dbe43932d
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-11-27 21:13:23 -0300

    advance newImg uploader

--- src/pages/dashboard/pages/Products.jsx
+++ src/pages/dashboard/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { TableRow } from "./index";
 import { Modal } from "src/components";
@@ -10,7 +10,13 @@ const colsTitles = ["id", "nombre", "precio", "ultima actualización", "publicad
 export function Products() {
   const navigate = useNavigate();
   const { products } = useLoaderData();
+  console.log(products);
   const [modal, setModal] = useState(false);
+  const [newImgs, setNewImgs] = useState(false);
+
+  useEffect(() => {
+    console.log(newImgs);
+  }, [newImgs]);
 
   async function uploadProductImage(file, productId, setLoader) {
     try {
@@ -47,10 +53,76 @@ export function Products() {
       console.log(e);
     }
   }
+  function handleImages({ target }) {
+    const files = target.files;
+    if (!files) return;
+    const rawImgs = {};
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (!file) continue;
 
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        rawImgs[file.name] = {
+          originalName: file.name,
+          URL: typeof reader.result === "string" ? reader.result : "",
+          file: file,
+          size: file.size,
+        };
+      };
+      reader.readAsDataURL(file);
+    }
+    console.log(rawImgs);
+    setNewImgs(rawImgs);
+  }
   return (
     <main className="w-full">
       <Modal isOpen={Boolean(modal)} onClose={() => setModal(false)}>
+        <div className="flex flex-col items-center ">
+          {/* <button
+            onClick={() => deleteProductImg(modal.productId)}
+            className="mx-auto my-2 flex items-center justify-center gap-2 rounded-lg border-2 border-red-500 bg-slate-800 p-1"
+          >
+            <i className="ri-delete-bin-2-fill self-center text-2xl capitalize text-red-500"></i>
+            <h1> Presione para borrar las imagenes</h1>
+          </button> */}
+          {modal?.prevImgs?.length && (
+            <div className="flex border-2 text-center">
+              <h1>Imganes previas: </h1>
+              {modal.prevImgs.map((i, index) => {
+                return <img className="w-[75px]" key={index} src={i} alt="" />;
+              })}
+            </div>
+          )}
+
+          <div className="flex border-2 text-center">
+            {newImgs ? (
+              Object.values(newImgs).map((i, index) => {
+                return <img className="w-[120px]" key={index} src={i.URL} alt="" />;
+              })
+            ) : (
+              <h1>No hay imagenes para este producto</h1>
+            )}
+          </div>
+          <label
+            htmlFor={`fileInput-${modal.productId}`}
+            className="mt-8 w-fit cursor-pointer  rounded border-2 bg-blue px-5 py-3 text-white hover:font-medium"
+          >
+            Cargar imagenes
+            <input
+              type="file"
+              id={`fileInput-${modal.productId}`}
+              accept="image/*"
+              multiple
+              title="Cargar imagenes"
+              className="hidden"
+              onChange={handleImages}
+            />
+          </label>
+        </div>
+      </Modal>
+      {/* <Modal isOpen={Boolean(modal)} onClose={() => setModal(false)}>
         <button
           onClick={() => deleteProductImg(modal.productId)}
           className="mx-auto my-2 flex items-center justify-center gap-2 rounded-lg border-2 border-red-500 bg-slate-800 p-1"
@@ -58,8 +130,7 @@ export function Products() {
           <i className="ri-delete-bin-2-fill self-center text-4xl capitalize text-red-500"></i>
           <h1> Presione para borrar</h1>
         </button>
-        <img src={modal.path} />
-      </Modal>
+        <img src={modal.path} /> */}
       <table className="my-4 w-full text-white">
         <thead className="border border-gold">
           <tr className="goldGradient text-base uppercase">
@@ -116,20 +187,8 @@ export function Products() {
                   />
                 ) : (
                   <TableRow
-                    content={
-                      <label
-                        htmlFor={`fileInput${id}`}
-                        className={`${loader ? "icons ri-loader-2-line text-2xl" : "icons ri-image-2-fill text-2xl"}`}
-                      >
-                        <input
-                          type="file"
-                          id={`fileInput${id}`}
-                          accept="image/*"
-                          className="hidden"
-                          onChange={(e) => uploadProductImage(e.target.files[0], id, setLoader)}
-                        />
-                      </label>
-                    }
+                    onClick={() => setModal({ prevImgs: images, productId: id })}
+                    content={<i className="ri-image-2-fill text-2xl"></i>}
                   />
                 )}
               </tr>
@@ -139,4 +198,25 @@ export function Products() {
       </table>
     </main>
   );
+}
+
+function UploadProductImg() {
+  return {
+    /* <TableRow
+      content={
+        <label
+          htmlFor={`fileInput${id}`}
+          className={`${loader ? "icons ri-loader-2-line text-2xl" : "icons ri-image-2-fill text-2xl"}`}
+        >
+          <input
+            type="file"
+            id={`fileInput${id}`}
+            accept="image/*"
+            className="hidden"
+            onChange={(e) => uploadProductImage(e.target.files[0], id, setLoader)}
+          />
+        </label>
+      }
+    /> */
+  };
 }
